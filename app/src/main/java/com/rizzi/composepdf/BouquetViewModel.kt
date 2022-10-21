6a846f550c798b57962707b931c5f9535baef596
Graziano Rizzi <graziano.rizzi1991@gmail.com>
2022-10-21 12:15:35 +0200

    - Divide PDFReader function in VerticalPdfReader and HorizontalPdfReader - Improve state handling, now in case of a remote resource or a Base64 pdf if the file has been already generated the state will be saved and restored as a Local resource type, that means the component no longer need to download again the pdf in case of a configuration change. - Now will be saved and restored the scroll position as well.

--- app/src/main/java/com/rizzi/composepdf/BouquetViewModel.kt
+++ app/src/main/java/com/rizzi/composepdf/BouquetViewModel.kt
@@ -1,8 +1,11 @@
 package com.rizzi.composepdf
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.rizzi.bouquet.HorizontalPdfReaderState
 import com.rizzi.bouquet.PdfReaderState
 import com.rizzi.bouquet.ResourceType
+import com.rizzi.bouquet.VerticalPdfReaderState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
@@ -10,9 +13,15 @@ class BouquetViewModel : ViewModel() {
     private val mStateFlow = MutableStateFlow<PdfReaderState?>(null)
     val stateFlow: StateFlow<PdfReaderState?> = mStateFlow
 
+    val switchState = mutableStateOf(false)
+
     fun openResource(resourceType: ResourceType) {
         mStateFlow.tryEmit(
-            PdfReaderState(resourceType, true)
+            if (switchState.value) {
+                HorizontalPdfReaderState(resourceType, true)
+            } else {
+                VerticalPdfReaderState(resourceType, true)
+            }
         )
     }
 
---
+++ bouquet/src/main/java/com/rizzi/bouquet/HorizontalPdfReaderState.kt
@@ -0,0 +1,57 @@
+package com.rizzi.bouquet
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.core.net.toUri
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.PagerState
+
+@OptIn(ExperimentalPagerApi::class)
+class HorizontalPdfReaderState(
+    resource: ResourceType,
+    isZoomEnable: Boolean = false,
+    internal val pagerState: PagerState = PagerState()
+) : PdfReaderState(resource, isZoomEnable) {
+
+    override val currentPage: Int
+        get() = pagerState.currentPage
+
+    override val isScrolling: Boolean
+        get() = pagerState.isScrollInProgress
+
+    companion object {
+        val Saver: Saver<HorizontalPdfReaderState, *> = listSaver(
+            save = {
+                val resource = it.file?.let { file ->
+                    ResourceType.Local(
+                        file.toUri()
+                    )
+                } ?: it.resource
+                listOf(
+                    resource,
+                    it.isZoomEnable,
+                    it.pagerState.currentPage
+                )
+            },
+            restore = {
+                HorizontalPdfReaderState(
+                    it[0] as ResourceType,
+                    it[1] as Boolean,
+                    PagerState(currentPage = it[2] as Int)
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun rememberHorizontalPdfReaderState(
+    resource: ResourceType,
+    isZoomEnable: Boolean = true
+): HorizontalPdfReaderState {
+    return rememberSaveable(saver = HorizontalPdfReaderState.Saver) {
+        HorizontalPdfReaderState(resource, isZoomEnable)
+    }
+}
