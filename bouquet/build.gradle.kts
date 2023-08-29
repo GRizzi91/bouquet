83a736fc96473a1ee099856b3c2d42c5b58f7c00
Aleš Kavčič <ales.kavcic86@gmail.com>
2023-08-29 10:02:33 +0200

    Revert "upgrade JAVA version"
    This reverts commit 2c6b1d30e2f7612c6c2ef1bde12c9a89f8e977f5.

--- bouquet/build.gradle.kts
+++ bouquet/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
